ca9df05bbd05c6910205841f53eb014d65ee1525
mmalenic <mmalenic1@gmail.com>
2026-01-22 14:30:50 +1100

    test: adjust jest config

--- infrastructure/postgres-manager/stage/jest.config.js
+++ infrastructure/postgres-manager/stage/jest.config.js
@@ -1,3 +1,6 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export const preset = 'ts-jest';
 export const testEnvironment = 'node';
+export const roots = ['<rootDir>/tests'];
+export const testMatch = ['**/*.test.ts'];
+export const transform = {
+  '^.+\\.tsx?$': 'ts-jest',
+};
